3d773b79772ce89f2d655047c56faa864f89e5e3
AronDJacobsen <59569696+AronDJacobsen@users.noreply.github.com>
2022-11-08 09:41:25 +0100

    model_update

--- models.py
+++ models.py
@@ -14,10 +14,165 @@ from pytorch_model_summary import summary
 # importing distributions
 import torch.distributions as dists
 
+class Encoder(nn.Module):
+    def __init__(self, encoder_net):
+        super(Encoder, self).__init__()
+
+        self.encoder = encoder_net
+
+    @staticmethod
+    def reparameterization(mu, log_var):
+        std = torch.exp(0.5*log_var)
+
+        eps = torch.randn_like(std)
+
+        return mu + std * eps
+
+    def encode(self, x):
+        h_e = self.encoder(x)
+        mu_e, log_var_e = torch.chunk(h_e, 2, dim=1)
+
+        return mu_e, log_var_e
+
+    def sample(self, x=None, mu_e=None, log_var_e=None):
+        if (mu_e is None) and (log_var_e is None):
+            mu_e, log_var_e = self.encode(x)
+        else:
+            if (mu_e is None) or (log_var_e is None):
+                raise ValueError('mu and log-var can`t be None!')
+        z = self.reparameterization(mu_e, log_var_e)
+        return z
+
+    def log_prob(self, x=None, mu_e=None, log_var_e=None, z=None):
+        if x is not None:
+            mu_e, log_var_e = self.encode(x)
+            z = self.sample(mu_e=mu_e, log_var_e=log_var_e)
+        else:
+            if (mu_e is None) or (log_var_e is None) or (z is None):
+                raise ValueError('mu, log-var and z can`t be None!')
+
+        return log_normal_diag(z, mu_e, log_var_e)
+
+    def forward(self, x, type='log_prob'):
+        assert type in ['encode', 'log_prob'], 'Type could be either encode or log_prob'
+        if type == 'log_prob':
+            return self.log_prob(x)
+        else:
+            return self.sample(x)
 
 
+class Decoder(nn.Module):
+    def __init__(self, decoder_net, distribution='categorical', num_vals=None):
+        super(Decoder, self).__init__()
+
+        self.decoder = decoder_net
+        self.distribution = distribution
+        self.num_vals = num_vals
+
+    def decode(self, z):
+        h_d = self.decoder(z)
+
+        if self.distribution == 'categorical':
+            b = h_d.shape[0]
+            d = h_d.shape[1] // self.num_vals
+            h_d = h_d.view(b, d, self.num_vals)
+            mu_d = torch.softmax(h_d, 2)
+            return [mu_d]
+
+        elif self.distribution == 'bernoulli':
+            mu_d = torch.sigmoid(h_d)
+            return [mu_d]
+
+        else:
+            raise ValueError('Either `categorical` or `bernoulli`')
+
+    def sample(self, z):
+        outs = self.decode(z)
+
+        if self.distribution == 'categorical':
+            mu_d = outs[0]
+            b = mu_d.shape[0]
+            m = mu_d.shape[1]
+            mu_d = mu_d.view(mu_d.shape[0], -1, self.num_vals)
+            p = mu_d.view(-1, self.num_vals)
+            x_new = torch.multinomial(p, num_samples=1).view(b, m)
+
+        elif self.distribution == 'bernoulli':
+            mu_d = outs[0]
+            x_new = torch.bernoulli(mu_d)
+
+        else:
+            raise ValueError('Either `categorical` or `bernoulli`')
+
+        return x_new
+
+    def log_prob(self, x, z):
+        outs = self.decode(z)
+
+        if self.distribution == 'categorical':
+            mu_d = outs[0]
+            log_p = log_categorical(x, mu_d, num_classes=self.num_vals, reduction='sum', dim=-1).sum(-1)
+
+        elif self.distribution == 'bernoulli':
+            mu_d = outs[0]
+            log_p = log_bernoulli(x, mu_d, reduction='sum', dim=-1)
+
+        else:
+            raise ValueError('Either `categorical` or `bernoulli`')
+
+        return log_p
+
+    def forward(self, z, x=None, type='log_prob'):
+        assert type in ['decoder', 'log_prob'], 'Type could be either decode or log_prob'
+        if type == 'log_prob':
+            return self.log_prob(x, z)
+        else:
+            return self.sample(x)
 
 
+class Prior(nn.Module):
+    def __init__(self, L):
+        super(Prior, self).__init__()
+        self.L = L
+
+    def sample(self, batch_size):
+        z = torch.randn((batch_size, self.L))
+        return z
+
+    def log_prob(self, z):
+        return log_standard_normal(z)
+
+
+class VAE(nn.Module):
+    def __init__(self, encoder_net, decoder_net, num_vals=256, L=16, likelihood_type='categorical'):
+        super(VAE, self).__init__()
+
+
+        self.encoder = Encoder(encoder_net=encoder_net)
+        self.decoder = Decoder(distribution=likelihood_type, decoder_net=decoder_net, num_vals=num_vals)
+        self.prior = Prior(L=L)
+
+        self.num_vals = num_vals
+
+        self.likelihood_type = likelihood_type
+
+    def forward(self, x, reduction='avg'):
+        # encoder
+        mu_e, log_var_e = self.encoder.encode(x)
+        z = self.encoder.sample(mu_e=mu_e, log_var_e=log_var_e)
+
+        # ELBO
+        RE = self.decoder.log_prob(x, z)
+        KL = (self.prior.log_prob(z) - self.encoder.log_prob(mu_e=mu_e, log_var_e=log_var_e, z=z)).sum(-1)
+
+        if reduction == 'sum':
+            return -(RE + KL).sum()
+        else:
+            return -(RE + KL).mean()
+
+    def sample(self, batch_size=64):
+        z = self.prior.sample(batch_size=batch_size)
+        return self.decoder.sample(z)
 
 
 
