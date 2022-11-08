9bfae4a19e5f1dee61e15ec699e807df0c7a0cb9
TECH-yufu <yuch0001@gmail.com>
2022-11-08 14:01:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=None)
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'])
 
+    # model parameters
+
+
     # tensorboard
     parser.add_argument('--log_dir', help='Store logs in this directory during training.',
                         default='runs', type=str)
@@ -20,21 +23,31 @@ if __name__ == '__main__':
 
     logger = Logger(directory=args.log_dir, comment="_HI_VAE", write=args.write)
 
+    ### load data
+    train_data = Boston(mode='train')
+    val_data = Boston(mode='val')
+    test_data = Boston(mode='test')
 
+    training_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+    val_loader = DataLoader(val_data, batch_size=64, shuffle=False)
+    test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
 
+    # create dummy dataloader to get a batch of 1
+    tester_loader = DataLoader(test_data, batch_size=1, shuffle=False)
 
-    ### test 
-    logger.log("Training using {}".format(args.device))
-
-    name = "Train"
-    for j in range(10):
-        scalars = {}
-        t = np.linspace(0, np.pi, 7)
-        y = lambda t: np.cos(t)+j
-        for i in t:
-            scalars[i] = y(i)
-
-        logger.write_to_board(name=name, scalars=scalars, index=j)
+    
+    # ### testing ###
+    # logger.log("Training using {}".format(args.device))
+    #
+    # name = "Train"
+    # for j in range(10):
+    #     scalars = {}
+    #     t = np.linspace(0, np.pi, 7)
+    #     y = lambda t: np.cos(t)+j
+    #     for i in t:
+    #         scalars[i] = y(i)
+    #
+    #     logger.write_to_board(name=name, scalars=scalars, index=j)
 
 
 
