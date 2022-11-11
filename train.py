1bc3d39256f9f689fed8fabc061edcdd6d25629a
TECH-yufu <yuch0001@gmail.com>
2022-11-11 13:14:21 +0100

    yml and training

---
+++ train.py
@@ -0,0 +1,44 @@
+def training(name, max_patience, num_epochs, model, optimizer, training_loader, val_loader):
+    nll_val = []
+    best_nll = 1000.
+    patience = 0
+
+    # Main loop
+    for e in range(num_epochs):
+        # TRAINING
+        model.train()
+        for indx_batch, batch in enumerate(training_loader):
+            if hasattr(model, 'dequantization'):
+                if model.dequantization:
+                    batch = batch + torch.rand(batch.shape)
+            loss = model.forward(batch)
+
+            optimizer.zero_grad()
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+        # Validation
+        loss_val = evaluation(val_loader, model_best=model, epoch=e)
+        nll_val.append(loss_val)  # save for plotting
+
+        if e == 0:
+            print('saved!')
+            torch.save(model, name + '.model')
+            best_nll = loss_val
+        else:
+            if loss_val < best_nll:
+                print('saved!')
+                torch.save(model, name + '.model')
+                best_nll = loss_val
+                patience = 0
+
+                samples_generated(name, val_loader, extra_name="_epoch_" + str(e))
+            else:
+                patience = patience + 1
+
+        if patience > max_patience:
+            break
+
+    nll_val = np.asarray(nll_val)
+
+    return nll_val
