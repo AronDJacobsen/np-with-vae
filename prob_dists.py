88fbc3da66a0442d6ff179a530e614a7412fe5c9
TECH-yufu <yuch0001@gmail.com>
2022-11-29 09:18:59 +0100

    Update prob_dists.py

--- prob_dists.py
+++ prob_dists.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
 import torch.nn.functional as F
+import math
+from torch import nn, Tensor
+from torch.distributions import Distribution
 
 PI = torch.from_numpy(np.asarray(np.pi))
 EPS = 1.e-5
@@ -14,6 +17,16 @@ def log_categorical(x, p, num_classes=256, reduction=None, dim=None):
         return torch.sum(log_p, dim)
     else:
         return log_p
+
+def categorical(x, p, num_classes=256, reduction=None, dim=None):
+    x_one_hot = F.one_hot(x.long(), num_classes=num_classes)
+    p = x_one_hot * (torch.clamp(p, EPS, 1. - EPS))
+    if reduction == 'avg':
+        return torch.mean(p, dim)
+    elif reduction == 'sum':
+        return torch.sum(p, dim)
+    else:
+        return p
 
 def log_bernoulli(x, p, reduction=None, dim=None):
     pp = torch.clamp(p, EPS, 1. - EPS)
@@ -46,3 +59,33 @@ def log_standard_normal(x, reduction=None, dim=None):
         return torch.sum(log_p, dim)
     else:
         return log_p
+
+### from Deep Learning:
+class ReparameterizedDiagonalGaussian(Distribution):
+    """
+    A distribution `N(y | mu, sigma I)` compatible with the reparameterization trick given `epsilon ~ N(0, 1)`.
+    """
+
+    def __init__(self, mu: Tensor, log_sigma: Tensor):
+        assert mu.shape == log_sigma.shape, f"Tensors `mu` : {mu.shape} and ` log_sigma` : {log_sigma.shape} must be of the same shape"
+        self.mu = mu
+        self.sigma = log_sigma.exp()
+
+    def sample_epsilon(self) -> Tensor:
+        """`\eps ~ N(0, I)`"""
+        return torch.empty_like(self.mu).normal_()
+
+    def sample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (without gradients)"""
+        with torch.no_grad():
+            return self.rsample()
+
+    def rsample(self) -> Tensor:
+        """sample `z ~ N(z | mu, sigma)` (with the reparameterization trick) """
+        # raise NotImplementedError # <- your code
+        return torch.distributions.normal.Normal(self.mu, self.sigma).rsample()
+
+    def log_prob(self, z: Tensor) -> Tensor:
+        """return the log probability: log `p(z)`"""
+        # raise NotImplementedError # <- your code
+        return torch.distributions.normal.Normal(self.mu, self.sigma).log_prob(z)
