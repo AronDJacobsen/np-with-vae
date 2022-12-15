dd72798d86cb54ba2280b15d9c661f89ca6d02a1
TECH-yufu <yuch0001@gmail.com>
2022-12-15 15:23:37 +0100

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#BSUB -J VAE_avocado
+#BSUB -J VAE_beta_4
 #BSUB -o VAE%J.out
 #BSUB -e VAE%J.err
 #BSUB -q gpuv100
@@ -13,9 +13,9 @@
 module load python3/3.9.6
 
 # load CUDA (for GPU support)
-module load cuda/11.3
+module load cuda/11.7
 
 # activate the virtual environment
 source DGM/bin/activate
 
-python main.py --seed 42 --device cuda --batch 16 --write
+python main.py --seed 3407 --device cuda --write --mode "traintest" --experiment "bank" --dataset "bank" --scale "normalize" --max_epochs 500 --max_patience 100 --prior "vampPrior" --beta 0.01
